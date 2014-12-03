972e308f7d7708572f9fa10568fb9bc95be615b6
Nicolas Lorier <n.lorier@gmail.com>
2014-12-03 13:06:41 +0100

    Ajout test_decompression

---
+++ src_etudiants/test_decompression.adb
@@ -0,0 +1,13 @@
+with decompression; use decompression;
+
+procedure test_decompression is
+
+	Nom_In : String := "VVV.txt";
+	Nom_Out : String := "DecompVVV.txt";
+	
+begin
+	
+	Decompresse (Nom_In, Nom_Out);
+
+end test_decompression;	
+	
