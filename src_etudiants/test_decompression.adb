cfae0ed91a04f6a74e351b9b9966d5a4f2b6f2e4
Nicolas Lorier <n.lorier@gmail.com>
2014-12-03 13:56:29 +0100

    Problemes de lecture fichier lors de test

--- src_etudiants/test_decompression.adb
+++ src_etudiants/test_decompression.adb
@@ -1,13 +1,36 @@
 with decompression; use decompression;
+with Ada.Streams.Stream_IO; use Ada.Streams.Stream_IO;
+with Ada.Integer_Text_Io; use Ada.Integer_Text_Io;
+with Ada.Text_Io; use Ada.Text_Io;
+with Ada.Float_Text_Io; use Ada.Float_Text_Io;
 
 procedure test_decompression is
 
 	Nom_In : String := "VVV.txt";
 	Nom_Out : String := "DecompVVV.txt";
 	
+	In_Fichier : Ada.Streams.Stream_IO.File_Type;
+	Flux : Ada.Streams.Stream_IO.Stream_Access;
+	
+	I : Integer;
+	
 begin
 	
-	Decompresse (Nom_In, Nom_Out);
+	Open(In_Fichier, In_File, Nom_In);
+	Flux := Stream(In_Fichier);
+
+	I := Integer'Input(Flux);
+	Put("Lecture Nb de caractères: ");
+	Put(I);
+	New_Line;
+	
+	While I > 0 loop
+		Put(Character'Input(Flux));
+		Put(Integer'Input(Flux));
+		New_Line;
+		I := I - 1;
+	end loop;
+--	Decompresse (Nom_In, Nom_Out);
 
 end test_decompression;	
 	
